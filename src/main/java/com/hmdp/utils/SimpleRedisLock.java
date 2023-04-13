642a0c7e319a8634dee51b8cb34e505756c8c371
wzw <809434255@qq.com>
2023-04-13 09:57:41 +0800

    Lua分布式锁

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,15 +1,31 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.stereotype.Component;
 
+import javax.print.attribute.standard.MediaSize;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
 
     private StringRedisTemplate stringRedisTemplate;
     private String LOCK_NAME;
-    private final String LOCK_PREFIX = "lock:";
+    private static final String LOCK_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String LOCK_NAME) {
         this.stringRedisTemplate = stringRedisTemplate;
@@ -23,7 +39,7 @@ public class SimpleRedisLock implements ILock {
      */
     @Override
     public boolean tryLock(Long timeoutSec) {
-        String value = Thread.currentThread().getId() + "";
+        String value = ID_PREFIX + Thread.currentThread().getId();
         Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(LOCK_PREFIX + LOCK_NAME, value, timeoutSec, TimeUnit.SECONDS);
         return BooleanUtil.isTrue(success);
     }
@@ -33,6 +49,13 @@ public class SimpleRedisLock implements ILock {
      */
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(LOCK_PREFIX + LOCK_NAME);
+        /*String value = stringRedisTemplate.opsForValue().get(LOCK_NAME);
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        if(threadId.equals(value)){
+            stringRedisTemplate.delete(LOCK_PREFIX + LOCK_NAME);
+        }*/
+
+        // 执行lua脚本，实现判断锁标识和删除锁的原子性
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(LOCK_PREFIX + LOCK_NAME), Collections.singletonList(ID_PREFIX + Thread.currentThread().getId()));
     }
 }
--- src/test/java/com/hmdp/test/CacheTest.java
+++ src/test/java/com/hmdp/test/CacheTest.java
@@ -3,17 +3,17 @@ package com.hmdp.test;
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
 @SpringBootTest
 public class CacheTest {
-    @Resource
-    ShopServiceImpl shopService;
+
 
     @Test
     public void test(){
-        shopService.saveShop2Redis(1L,30L);
+
     }
 }
